c84d9e58dc94e7a1026195e5dba7ee700f96f7b2
rkorona <korona.starx@gmail.com>
2026-06-20 11:43:38 +0800

    fix other income

--- app/src/main/java/com/example/myapplication/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/DashboardScreen.kt
@@ -20,7 +20,7 @@ import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.item
+
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
